4bc6f4e0a3be20e53b187a8b08f1f19e696bc9fb
Jacob Smith <jacob.smith@modernfold.com>
2021-08-10 08:14:15 -0400

    initial Check-in

---
+++ src/FavBooks.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+ 
+class FavBooks extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Favorite Books</h2>
+        <p>Holes By: Louis Sachar</p>
+        <p>Adventures of Huckleberry Finn By: Mark Twain</p>
+        <p>The Giving Tree By: Shel Silverstein</p>
+      </div>
+    );
+  }
+}
+ 
+export default FavBooks;
